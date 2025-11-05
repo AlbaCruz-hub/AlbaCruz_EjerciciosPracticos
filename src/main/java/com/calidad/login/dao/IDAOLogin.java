212d3fe532be90d4a63b299d3fdde09bba133b46
Alba Cruz <gabis200314@gmail.com>
2025-11-05 09:16:10 -0600

    Agrego nueva carpeta con archivos del proyecto

--- src/main/java/com/calidad/login/dao/IDAOLogin.java
+++ src/main/java/com/calidad/login/dao/IDAOLogin.java
@@ -1,7 +1,21 @@
 package com.calidad.login.dao;
 
+import java.util.List;
+
 import com.calidad.login.modelo.Usuario;
 
 public interface IDAOLogin {
-    Usuario findUserByEmail(String email);
+    Usuario findUsuarioByEmail(String email);
+
+    int save(Usuario u);
+
+    List<Usuario> findAll();
+    
+    boolean deleteById(int id);
+
+    Usuario updateUser(Usuario userOld);
+
+    Usuario findById(int id);
+
+    Usuario findByUsuarioName(String name);    
 }
--- src/main/java/com/calidad/login/modelo/Usuario.java
+++ src/main/java/com/calidad/login/modelo/Usuario.java
@@ -1,16 +1,12 @@
 package com.calidad.login.modelo;
 
 public class Usuario {
-private int id;
-private String name;
-private String email;
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private boolean isLogged;
 
-private String password;
-private boolean isLogged;
-
-    public boolean isLogged() {
-    return isLogged;
-}
     public Usuario(String email, boolean isLogged, String name, String password) {
         this.email = email;
         this.isLogged = isLogged;
@@ -18,43 +14,52 @@ private boolean isLogged;
         this.password = password;
     }
 
+    public boolean isLogged() {
+        return isLogged;
+    }
+
+    public void setLogged(boolean isLogged) {
+        this.isLogged = isLogged;
+    }
+
     public int getId() {
         return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public boolean isIsLogged() {
-        return isLogged;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public boolean isIsLogged() {
+        return isLogged;
     }
 
     public void setIsLogged(boolean isLogged) {
         this.isLogged = isLogged;
     }
+    
 }
